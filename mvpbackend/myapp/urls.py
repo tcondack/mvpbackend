5671cf16bbb7d64b0343f051b69880d7e4d9aa8b
Tiago <tiagocondack@gmail.com>
2025-10-13 21:41:47 -0300

    Update urls.py

--- mvpbackend/myapp/urls.py
+++ mvpbackend/myapp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('cadastro/', views.cadastro, name='cadastro'),
     path('login/', views.login, name='login'),
     path('logout_view', views.logout_view, name='logout_view'),
+    path('index', views.index, name='index'),
 
 ]
