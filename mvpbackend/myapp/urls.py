7061d48165712bd0e0764425dd296d367a2bfc97
Tiago <tiagocondack@gmail.com>
2025-10-20 23:03:43 -0300

    Add new URL path view infos

--- mvpbackend/myapp/urls.py
+++ mvpbackend/myapp/urls.py
@@ -3,9 +3,13 @@ from . import views
 
 urlpatterns = [
     path('teste/', views.teste, name='teste'),
+
+    #url administrativo
     path('cadastro/', views.cadastro, name='cadastro'),
     path('login/', views.login, name='login'),
     path('logout_view/', views.logout_view, name='logout_view'),
+
+    #url públicas
     path('parques/', views.parques, name='parques'),
     path('trilhas/', views.trilhas, name='trilhas'),
     path('eventos/', views.eventos, name='eventos'),
