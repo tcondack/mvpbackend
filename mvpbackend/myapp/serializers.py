3952c600f1698ed8852816716b63d0368d16047b
Tiago <tiagocondack@gmail.com>
2025-11-12 10:43:02 -0300

    updates gerais

--- mvpbackend/myapp/serializers.py
+++ mvpbackend/myapp/serializers.py
@@ -3,18 +3,18 @@ from .models import Parque, Trilhas, Eventos
 
 class ParqueSerializer(serializers.ModelSerializer):
   """ Serializer para o modelo Parque"""
-class Meta:
-  model = Parque
-  fields = '__all__'
+  class Meta:
+    model = Parque
+    fields = '__all__'
 
 class TrilhasSerializer(serializers.ModelSerializer):
   """ Serializer para o modelo Trilhas"""
-class Meta:
-  model = Trilhas
-  fields = '__all__'
+  class Meta:
+    model = Trilhas
+    fields = '__all__'
 
 class EventosSerializer(serializers.ModelSerializer):
   """ Serializer para o modelo Eventos"""
-class Meta:
-  model = Eventos
-  fields = '__all__'
+  class Meta:
+    model = Eventos
+    fields = '__all__'
